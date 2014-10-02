d07aa120e3a99ccaa93a107c0e50ed779f3e897e
Gulsah Kose <gulsah.1004@gmail.com>
2014-10-02 03:32:07 +0300

    Inital commit

---
+++ arphunting.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w
+use strict;
+	
+print "Please enter network address: ";
+chomp(my $net = <STDIN>);
+
+print "Please enter subnet: ";
+chomp(my $netmask = <STDIN>);
+
+my $subnet = $net."/".$netmask;
+print $subnet;
+	
+my $file_new = "arpscan_output.txt";
+my $file_old = "arpscan_output2.txt";
+	
+if(-e $file_new) 
+{
+	if(!-e $file_old)
+	{
+		`mv $file_new $file_old`;
+	}
+} 
+elsif(-e $file_new && -e $file_old)
+{
+	`rm -rf $file_old`;
+	`mv $file_new $file_old`;
+}
+
+`sudo arp-scan --interface=wlan0 '$subnet' > $file_new`;
